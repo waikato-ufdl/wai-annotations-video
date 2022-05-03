95a265d8044711e889523d70450c50ea669520e2
Peter Reutemann <fracpete@gmail.com>
2022-05-03 16:58:28 +1200

    added from_frame/to_frame/max_frames options

--- src/wai/annotations/video/format/video_file/component/_VideoFileReader.py
+++ src/wai/annotations/video/format/video_file/component/_VideoFileReader.py
@@ -20,11 +20,32 @@ class VideoFileReader(
         help="the video file to read"
     )
 
-    nth_frame: str = TypedOption(
+    from_frame: int = TypedOption(
+        "-f", "--from-frame",
+        type=int,
+        default=1,
+        help="determines with which frame to start the stream (1-based index)"
+    )
+
+    to_frame: int = TypedOption(
+        "-t", "--to-frame",
+        type=int,
+        default=-1,
+        help="determines after which frame to stop (1-based index); ignored if <=0"
+    )
+
+    nth_frame: int = TypedOption(
         "-n", "--nth-frame",
         type=int,
         default=1,
         help="determines whether frames get skipped and only evert nth frame gets forwarded"
+    )
+
+    max_frames: int = TypedOption(
+        "-m", "--max-frames",
+        type=int,
+        default=-1,
+        help="determines the maximum number of frames to read; ignored if <=0"
     )
 
     """
@@ -46,6 +67,7 @@ class VideoFileReader(
         if not hasattr(self, "_cap"):
             self._cap = cv2.VideoCapture(self.input_file)
             self._frame_no = 0
+            self._frame_count = 0
 
         # next frame?
         count = 0
@@ -56,9 +78,23 @@ class VideoFileReader(
             retval, frame_curr = self._cap.read()
 
             if retval:
+                # within frame window?
+                if self.from_frame > 0:
+                    if self._frame_no < self.from_frame:
+                        continue
+                if self.to_frame > 0:
+                    if self._frame_no >= self.to_frame:
+                        break
+
                 # skip frame?
                 if (self.nth_frame > 1) and (count < self.nth_frame):
                     continue
+
+                # max frames reached?
+                if (self.max_frames > 0) and (self._frame_count >= self.max_frames):
+                    break
+
+                self._frame_count += 1
                 count = 0
                 data = cv2.imencode(".jpg", frame_curr)[1].tobytes()
                 filename = "%08d.jpg" % self._frame_no
